b36d91f4efcf6d3b39dd5441f377a0427c9d2fa9
kritika-kishore <71135912+kritika-kishore@users.noreply.github.com>
2022-05-24 19:27:45 +0530

    Adding all changes at once instead of multiple commits

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # TDD-module
 
 Using jest for unit testing as the code has been written in javascript.
+
+Note that in the last condition where the application throws a custom exception, we can add try-catch block in the sum.js code to check that issue. But then jest will fail that test saying no exception was thrown -- that exception was already handled in the code, so there was nothing for jest test case to catch.
--- js/Testing/sum.test.js
+++ js/Testing/sum.test.js
@@ -1,8 +1,47 @@
-import sum from './sum'
+const add = require('../sum.js')
 
-test('Properly adds 2 numbers', () => {
+test('Returns 0 for empty string', () => {
     expect (
-        sum("","")
+        add("")
     ).toBe(0)
+})
 
+test('Returns the number for 1 number passed', () => {
+    expect (
+        add("1")
+    ).toBe(1)
+})
+
+test('Returns sum of numbers', () => {
+    expect (
+        add("225,75")
+    ).toBe(300)
+})
+
+test('Returns incorrect format', () => {
+    expect (
+        add("225,abc")
+    ).toBe("Incorrect input format")
+})
+
+test('Returns sum of numbers', () => {
+    expect (
+        add("1,2,3,4")
+    ).toBe(10)
+})
+
+test('Returns sum of numbers with delimiters: , and \n', () => {
+    expect (
+        add("1,2,3\n4")
+    ).toBe(10)
+})
+
+test('Returns sum of numbers with custom delimiter', () => {
+    expect (
+        add("//;\n1;2")
+    ).toBe(3)
+})
+
+test('Negetive numbers are not allowed', () => {
+    expect(() => {add("-1,4")}).toThrow(Error("Negetive numbers are not allowed :-1"))
 })
